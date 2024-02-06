4c21caf72cc149fbbc627ffcfa6c9d2939b33bff
Vova <tarabanvv@mail.ru>
2024-02-06 12:44:26 +0800

    class HW

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
-import TikTacToe from './components/ticTacToe/TikTacToe';
+import { OldTikTacToe } from './components/ticTacToe/TikTacToe';
 
 export const App = () => {
-	return <TikTacToe />;
+	return (
+		<>
+			<OldTikTacToe />
+		</>
+	);
 };
--- src/components/ticTacToe/TikTacToe.js
+++ src/components/ticTacToe/TikTacToe.js
@@ -1,89 +1,118 @@
 import './TikTacToe.css';
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const TikTacToe = () => {
-	const { isCircle, arr, win, winCircle, winClose } = useSelector(
-		(state) => state
-	);
-	const dispatch = useDispatch();
-	const winConditions = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6]
-	];
+export class OldTikTacToeContainer extends Component {
+	constructor(props) {
+		super(props);
 
-	const checkWin = () => {
-		winConditions.forEach((condition) => {
-			let boolClose = condition.every((element) => winClose.includes(element));
+		this.state = {
+			winConditions: [
+				[0, 1, 2],
+				[3, 4, 5],
+				[6, 7, 8],
+				[0, 3, 6],
+				[1, 4, 7],
+				[2, 5, 8],
+				[0, 4, 8],
+				[2, 4, 6]
+			]
+		};
+	}
+
+	reset = () => {
+		this.props.dispatch({ type: 'SET_ALL' });
+	};
+
+	checkWin = () => {
+		this.state.winConditions.forEach((condition) => {
+			let boolClose = condition.every((element) =>
+				this.props.winClose.includes(element)
+			);
 			let boolCircle = condition.every((element) =>
-				winCircle.includes(element)
+				this.props.winCircle.includes(element)
 			);
 
 			if (boolCircle) {
-				dispatch({ type: 'SET_WIN', payload: 'Победил нолик' });
+				this.props.dispatch({ type: 'SET_WIN', payload: 'Победил нолик' });
 				return;
 			}
 			if (boolClose) {
-				dispatch({ type: 'SET_WIN', payload: 'Победил крестик' });
+				this.props.dispatch({ type: 'SET_WIN', payload: 'Победил крестик' });
 				return;
 			}
 		});
 
-		let bool = arr.includes(null);
-		if (!bool && !win) {
-			dispatch({ type: 'SET_WIN', payload: 'Ничья' });
+		let bool = this.props.arr.includes(null);
+		if (!bool && !this.props.win) {
+			this.props.dispatch({ type: 'SET_WIN', payload: 'Ничья' });
 		}
 	};
 
-	const onClickBtn = (i) => {
-		const newArr = [...arr];
+	onClickBtn = (i) => {
+		const newArr = [...this.props.arr];
 		if (newArr[i] !== 'circle' && newArr[i] !== 'close') {
-			newArr[i] = isCircle ? 'circle' : 'close';
+			newArr[i] = this.props.isCircle ? 'circle' : 'close';
 			if (newArr[i] === 'close') {
-				dispatch({ type: 'SET_WIN_CLOSE', payload: [...winClose, i] });
-				dispatch({ type: 'SET_ARR', payload: newArr });
-				dispatch({ type: 'SET_IS_CIRCLE', payload: !isCircle });
+				this.props.dispatch({
+					type: 'SET_WIN_CLOSE',
+					payload: [...this.props.winClose, i]
+				});
+				this.props.dispatch({ type: 'SET_ARR', payload: newArr });
+				this.props.dispatch({
+					type: 'SET_IS_CIRCLE',
+					payload: !this.props.isCircle
+				});
 			} else if (newArr[i] === 'circle') {
-				dispatch({ type: 'SET_WIN_CIRCLE', payload: [...winCircle, i] });
-				dispatch({ type: 'SET_ARR', payload: newArr });
-				dispatch({ type: 'SET_IS_CIRCLE', payload: !isCircle });
+				this.props.dispatch({
+					type: 'SET_WIN_CIRCLE',
+					payload: [...this.props.winCircle, i]
+				});
+				this.props.dispatch({ type: 'SET_ARR', payload: newArr });
+				this.props.dispatch({
+					type: 'SET_IS_CIRCLE',
+					payload: !this.props.isCircle
+				});
 			}
-			checkWin();
+			this.checkWin();
 		}
 	};
 
-	const reset = () => {
-		dispatch({ type: 'SET_ALL' });
-	};
+	componentDidUpdate() {
+		this.checkWin();
+	}
 
-	React.useEffect(() => {
-		checkWin();
-	}, [winCircle, winClose]);
+	render() {
+		return (
+			<>
+				<h1>Следующий ход у {this.props.isCircle ? 'нолика' : 'крестика'}</h1>
+				<div className='wrapper'>
+					{this.props.arr.map((elem, i) => {
+						return (
+							<button
+								onClick={() => this.onClickBtn(i)}
+								key={i}
+								disabled={this.props.win}
+							>
+								<div className={elem}></div>
+							</button>
+						);
+					})}
+				</div>
+				{this.props.win ? <h1>{this.props.win}</h1> : null}
+				<button onClick={this.reset} className='reset'>
+					Начать заново
+				</button>
+			</>
+		);
+	}
+}
+const mapStateToProps = (state) => ({
+	isCircle: state.isCircle,
+	arr: state.arr,
+	win: state.win,
+	winCircle: state.winCircle,
+	winClose: state.winClose
+});
 
-	return (
-		<>
-			<h1>Следующий ход у {isCircle ? 'нолика' : 'крестика'}</h1>
-			<div className='wrapper'>
-				{arr.map((elem, i) => {
-					return (
-						<button onClick={() => onClickBtn(i)} key={i}>
-							<div className={elem}></div>
-						</button>
-					);
-				})}
-			</div>
-			{win ? <h1>{win}</h1> : null}
-			<button onClick={reset} className='reset'>
-				Начать заново
-			</button>
-		</>
-	);
-};
-
-export default TikTacToe;
+export const OldTikTacToe = connect(mapStateToProps)(OldTikTacToeContainer);
